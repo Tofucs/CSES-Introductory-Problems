03e971f5b47b6da75b29570b4ce7ce1004aa64a1
Andrew Hu <94906754+Tofucs@users.noreply.github.com>
2021-12-25 17:22:23 -0500

    Update Permutations.cpp

--- IntroductoryProblems/Permutations/Permutations.cpp
+++ IntroductoryProblems/Permutations/Permutations.cpp
@@ -4,7 +4,9 @@
 int main() {
   int n;
   std::cin >> n;
-  if (n < 4) 
+  if (n == 1) 
+    std::cout << "1";
+  else if (n < 4) 
     std::cout << "NO SOLUTION";
   else if (n == 4)
     std::cout << "2 4 1 3\n";
