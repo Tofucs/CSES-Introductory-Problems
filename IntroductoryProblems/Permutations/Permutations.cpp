12ecb474efa206d8c8b1cc2a0248d20a9d98f60a
Andrew Hu <94906754+Tofucs@users.noreply.github.com>
2021-12-25 17:20:47 -0500

    lmao this shit

---
+++ IntroductoryProblems/Permutations/Permutations.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+
+int main() {
+  int n;
+  std::cin >> n;
+  if (n < 4) 
+    std::cout << "NO SOLUTION";
+  else if (n == 4)
+    std::cout << "2 4 1 3\n";
+  else {
+    for (int i = 1; i <= n; i += 2)
+      std::cout << i << " ";
+    for (int i = 2; i <= n; i += 2)
+      std::cout << i << " ";
+  }
+  
+}
